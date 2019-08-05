5fe9dfd6f0b8d74b545871d621223f30bfaba570
HenriqueFutema <minifut0407@gmail.com>
2019-08-04 21:15:43 -0300

    Commit final

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,16 +1,12 @@
-const users = [
-    { id: 1, name: 'Henrique', email: 'henrique.futema01@gmail.com' },
-    { id: 2, name: 'Test', email: 'test@gmail.com' }
-
-]
+const User = require('./models/User')
 
 module.exports = {
     Query: {
-        users: () => users,
-        user: () => users[0]
+        users: () => User.find(),
+        user: (_, { id }) => User.findById(id)
     },
 
     Mutation: {
-        createUser: () => users[0]
+        createUser: (_, { name, email }) => User.create({ name, email })
     }
 }
